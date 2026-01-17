c0a47d1efb1ded22bbe332fa7bbd9850daf9a9fc
gabriel-melgaco <gabrielmelgacom@gmail.com>
2026-01-17 17:57:00 -0300

    (refactor(storage): atualização do endpoint de Upload de Arquivos

--- backend/storage/urls.py
+++ backend/storage/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import GeneratePresignedUrlView
 
 urlpatterns = [
-    path("upload_image/presigned-url/", GeneratePresignedUrlView.as_view()),
+    path("upload/presigned-url/", GeneratePresignedUrlView.as_view()),
 ]
